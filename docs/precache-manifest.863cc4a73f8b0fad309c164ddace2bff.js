b4736e86a1eef049bcb0cdaa9fb904754566b992
xLeDocteurx <lenoirc.fr@gmail.com>
2020-01-06 17:02:55 +0100

    github build in docs folder

---
+++ docs/precache-manifest.863cc4a73f8b0fad309c164ddace2bff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8210ea8e43deec6b99536f1ac6a79c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "698da716e9dc9b28d4e4",
+    "url": "/static/css/main.d0f4b2c6.chunk.css"
+  },
+  {
+    "revision": "66fe81aa952d4e35fc0a",
+    "url": "/static/js/2.42007ad1.chunk.js"
+  },
+  {
+    "revision": "e25222ef4993b16ce4830064af33d09f",
+    "url": "/static/js/2.42007ad1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "698da716e9dc9b28d4e4",
+    "url": "/static/js/main.8e20fd8f.chunk.js"
+  },
+  {
+    "revision": "7a79c40c11fc2417b500",
+    "url": "/static/js/runtime-main.904fee96.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
